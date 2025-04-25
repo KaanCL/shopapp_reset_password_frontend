d03008f25f1d0b233604cf0845c9da80ddb86cf3
KaanCL <kaancerkez34@outlook.com>
2025-04-25 11:17:04 +0300

    🐛 Fix: Bugs solved

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
 "use client";
-import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
+import React, { Suspense, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useSearchParams } from "next/navigation";
 import ResetPasswordForm from "@/components/reset_password_form";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const accessToken = searchParams.get('access_token');
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-        <ResetPasswordForm accessToken={accessToken} />
-      </div>
+      <ResetPasswordFormWithSearchParams />
     </Suspense>
   );
 }
+
+const ResetPasswordFormWithSearchParams = () => {
+  const searchParams = useSearchParams();
+  const accessToken = searchParams.get("access_token");
+
+  return <ResetPasswordForm accesToken={accessToken} />;
+};
