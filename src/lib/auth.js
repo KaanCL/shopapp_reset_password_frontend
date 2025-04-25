94d30b1e4277fd2b007f615002b372d0e584c479
KaanCL <kaancerkez34@outlook.com>
2025-04-25 13:33:53 +0300

    🐛 Fix : Resolved the Supabase module import issue.

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,11 @@
 
-import { supabase } from './supabaseClient';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 export const setSessionWithToken = async (accessToken) => {
   return await supabase.auth.setSession({
